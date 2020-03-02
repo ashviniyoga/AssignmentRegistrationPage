a4ed5206228972d4f007fa9c6704ad47ae201ffa
ashvini <deepabharath0306@gmail.com>
2020-03-01 23:42:02 -0800

    Project created with dependency.

---
+++ AssignmentRegistration/src/main/java/com/assignmt/AssignmentRegistrationApplication.java
@@ -0,0 +1,14 @@
+package com.assignmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentRegistrationApplication.class, args);
+		System.out.println("Hello welcome to Spring boot");
+	}
+
+}
---
+++ AssignmentRegistration/src/test/java/com/assignmt/AssignmentRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
